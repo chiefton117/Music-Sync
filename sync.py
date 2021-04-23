e0c90e6266e143ba94c96d2501985d0ef3280735
chiefton117 <timjharrold@gmail.com>
2021-04-23 01:02:52 -0400

    Text fun
    -Added ascii text box method
    - Bug fixing in main walkthrough

--- sync.py
+++ sync.py
@@ -10,6 +10,7 @@
 ## Each directory should have an artist->album->song folder structure, and each will be checked for their string values
 ## Arguments given will be the function(backup, sync), source and destination directories
 ##
+import sys
 from filecmp import dircmp
 import os.path
 from shutil import copytree
@@ -21,28 +22,31 @@ def main():
 	source = tk.Tk()
 	source.withdraw()
 
+	asciibox("Music Synchronizer V1.0", "#")
+
 	# Select two guaranteed existing source directories
 	print("Please select a source directory | back up from here");
 	source = filedialog.askdirectory(title="--Select Source Directory--", mustexist = True)
+	if(len(source) < 1): 
+		print("Cancelling...");
+		sys.exit();
 
 	print("Please select a destination directory | back up to here");
 	destination = filedialog.askdirectory(title="--Select Destination Directory--", mustexist = True)
+	if(len(destination) < 1): 
+		print("Cancelling...");
+		sys.exit();
 
 	print("Back up from " + source + " ----> " + destination);
-	return None if tolower(input("Y/N")) != "y";
+	sys.exit() if input("Y/N: ").lower() != "y" else backup(source, destination);
+	
+	
 
-	#source = input("")
-	dir1 = "E:\\Music";
-	dir2 = "C:\\Users\\Luna\\Music";
-	comp = dircmp(dir1,dir2); # Create the comparison object
-	for a in comp.left_only:
-		print(os.path.join(comp.left,a))
-
-# Converts your music to a logical artist->album->song structure, given the metadata of songs in your directories
-# For each artist, recursively find subdirectories until we hit a file
-#def cleanStructure():
-
-
+	#dir1 = "E:\\Music";
+	#dir2 = "C:\\Users\\Luna\\Music";
+	#comp = dircmp(dir1,dir2); # Create the comparison object
+	#for a in comp.left_only:
+	#	print(os.path.join(comp.left,a))
 
 # Backup from one directory to another, given two guaranteed existing directories in these steps:
 # 1. Copy all artists only existing in source to directory
@@ -91,8 +95,8 @@ def backup(s, d):
 	}
 	print("{:<8} {:<15} {:<10} {:<10}".format('Format', 'Success','Error','Accuracy'))
 	for k, v in accuracy.items():
-	form, success, error, acc = v
-	print ("{:<8} {:<15} {:<10} {:<10}".format(form, success, error, acc))
+		form, success, error, acc = v
+		print ("{:<8} {:<15} {:<10} {:<10}".format(form, success, error, acc))
 
 
 	print(len(comp.funny_files) + " Anomalies Found! Listing...")
@@ -102,5 +106,15 @@ def backup(s, d):
 
 # Synchronize two guaranteed existing directories
 #def synchronize(s, d):
+
+def asciibox(text, symbol):
+	length = len(text) + 6; # Length of word + 3 spaces per side
+	
+	space = " ";
+	print((symbol * 5) + (space * (len(text)-2)) + (symbol * 5))
+	print(symbol + (space * length) + symbol)
+	print(symbol + (space*3) + text + (space*3) + symbol)
+	print(symbol + (space * length) + symbol)
+	print((symbol * 5) + (space * (len(text)-2)) + (symbol * 5))
 
 main();
